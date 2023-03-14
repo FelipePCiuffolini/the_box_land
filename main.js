d3e744a39bd55791304eddf108b46bea1339b76a
Felipe Polzella <dpolzella18@gmail.com>
2023-03-14 13:13:26 -0300

    addying-javascript-begin

---
+++ main.js
@@ -0,0 +1,8 @@
+let miNombre = prompt("Cual es tu nombre?");
+console.log(miNombre);
+let profesion = "medico";
+//alert("hola " + miNombre + " de profesión " + profesion)
+let edad = prompt("Cuantos años tenes?");
+let edadVieja = prompt("Cuantos años tuviste antes?");
+console.log(parseInt (edadVieja) + parseInt (edad));
+alert(`hola ${miNombre} de profesion: ${profesion}`);
